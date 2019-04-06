1cc1b4e9845add16b0fcbe31181d6ad8680bd212
Joao Pedro <jguedespresto@gmail.com>
2019-04-06 19:00:57 -0300

    Página, Bean e DAO Usuario

---
+++ Livraria/src/java/bean/usuarioBean.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import model.Usuario;
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import dao.UsuarioDAO;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.ServletContext;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+
+/**
+ *
+ * @author John Peter
+ */
+@ManagedBean
+@ViewScoped
+public class usuarioBean {
+    
+    Usuario usuario = new Usuario();
+
+    List usuarios = new ArrayList();
+
+    //construtor
+    public usuarioBean() {
+        usuarios = new UsuarioDAO().buscarTodas();
+        usuario = new Usuario();
+    }
+
+    //Métodos dos botões 
+    public void record(ActionEvent actionEvent) {
+        new UsuarioDAO().persistir(usuario);
+        usuarios = new UsuarioDAO().buscarTodas();
+        usuario = new Usuario();
+    }
+
+    public void exclude(ActionEvent actionEvent) {
+        new UsuarioDAO().remover(usuario);
+        usuarios = new UsuarioDAO().buscarTodas();
+        usuario = new Usuario();
+    }
+
+    //getters and setters
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List usuarios) {
+        this.usuarios = usuarios;
+    }      
+    
+    public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow header = sheet.getRow(0);
+
+        HSSFCellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
+        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
+            HSSFCell cell = header.getCell(i);
+
+            cell.setCellStyle(cellStyle);
+        }
+    }
+
+    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+        pdf.open();
+        pdf.setPageSize(PageSize.A4);
+
+        ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+    }    
+    
+}
---
+++ Livraria/src/java/dao/UsuarioDAO.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Usuario;
+import util.PersistenceUtil;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author John Peter
+ */
+public class UsuarioDAO implements Serializable{
+    
+    public static UsuarioDAO usuarioDAO;
+
+    public static UsuarioDAO getInstance() {
+        if (usuarioDAO == null) {
+            usuarioDAO = new UsuarioDAO();
+        }
+        return usuarioDAO;
+    }
+    
+    public Usuario buscar(String nome) {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        Query query = em.createQuery("select a from Usuario a where a.nomeUsuario =:nome ");
+        query.setParameter("nome", nome);
+
+        List<Usuario> usuario = query.getResultList();
+        if (usuario != null && usuario.size() > 0) {
+            return usuario.get(0);
+        }
+
+        return null;
+    }
+
+    public List<Usuario> buscarTodas() {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        Query query = em.createQuery("from Usuario As a");
+        return query.getResultList();
+    }
+
+    public List<Usuario> buscarTbEditoraInstancia() {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        Query query = em.createQuery("select distinct a from Usuario a group by a.usuario");
+        return query.getResultList();
+    }
+    
+    public void remover(Usuario usuario) {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        em.getTransaction().begin();
+        if (!em.contains(usuario)) {
+            usuario = em.merge(usuario);
+        }
+        em.remove(usuario);
+        em.getTransaction().commit();
+    }
+
+    public Usuario persistir(Usuario usuario) {
+        EntityManager em = PersistenceUtil.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            usuario = em.merge(usuario);
+            em.getTransaction().commit();
+            System.out.println("Registro Usuario gravado com sucesso");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+
+    public void removeAll() {
+       EntityManager em = PersistenceUtil.getEntityManager();
+       em.getTransaction().begin();
+       Query query = em.createQuery(" delete from Usuario ");
+       query.executeUpdate();
+       em.getTransaction().commit();
+    }
+    
+}
